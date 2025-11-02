6e149ed2b953459dc8f369ce1b42b383728062df
Bahaa montaser <bebo18044@gmail.com>
2025-11-02 23:52:17 +0200

    add all files pro 0 and 1

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,18 +3,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from '../Header';
-import MainContent from '../MainContent';
-import Footer from '../Footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header /> 
-    <MainContent />
-    <Footer />
+     <WelcomeMessage /> 
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -35,7 +31,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-       <WelcomeMessage />
     </>
   )
 }
