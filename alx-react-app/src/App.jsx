789bf0160755e4b4af4c92a027dd4a0940289ad9
Bahaa montaser <bebo18044@gmail.com>
2025-11-09 13:34:12 +0200

    add task 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
@@ -9,6 +10,26 @@ function App() {
     <>
       <Header />
       <MainContent />
+      
+      {/* UserProfile Components with different props */}
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+      
+      <UserProfile 
+        name="Bob" 
+        age="30" 
+        bio="Software developer and coffee enthusiast" 
+      />
+      
+      <UserProfile 
+        name="Charlie" 
+        age="28" 
+        bio="Travel blogger and foodie" 
+      />
+      
       <Footer />
       <WelcomeMessage />
     </>
