cbf1eecd6e369fa8dece72a7c8fdb6e971afc707
Bahaa montaser <bebo18044@gmail.com>
2025-11-09 16:23:30 +0200

    my company files

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const navStyle = {
+    display: "flex",
+    gap: "20px",
+    backgroundColor: "#333",
+    padding: "10px",
+  };
+
+  const linkStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
+
+  return (
+    <nav style={navStyle}>
+      <Link style={linkStyle} to="/">Home</Link>
+      <Link style={linkStyle} to="/about">About</Link>
+      <Link style={linkStyle} to="/services">Services</Link>
+      <Link style={linkStyle} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
