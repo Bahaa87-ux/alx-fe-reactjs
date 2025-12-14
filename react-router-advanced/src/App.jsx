3b5c88661197f14490c215f80b8871bc0c4ec105
Bahaa montaser <bebo18044@gmail.com>
2025-12-14 22:45:09 +0200

    fix brwos

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,17 +1,37 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Blog from './pages/Blog';
 import ProtectedRoute from './components/ProtectedRoute';
 
-const isAuthenticated = true; // أو false لتجربة redirect
+const isAuthenticated = true;
 
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/about" element={<About />} />
-  <Route
-    path="/profile/*"
-    element={
-      <ProtectedRoute isAuthenticated={isAuthenticated}>
-        <Profile />
-      </ProtectedRoute>
-    }
-  />
-  <Route path="/blog/:id" element={<Blog />} />
-</Routes>
+function App() {
+  return (
+    <BrowserRouter>
+      <nav style={{ marginBottom: '20px' }}>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/about">About</Link> |{' '}
+        <Link to="/profile">Profile</Link> |{' '}
+        <Link to="/blog/1">Blog 1</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
